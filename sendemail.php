b6b170dce3dbbb73a5ea5e8c6b709330627a4feb
R-Habash <59250931+R-Habash@users.noreply.github.com>
2020-01-03 21:22:09 +0200

    SENDING EMAIL

--- sendemail.php
+++ sendemail.php
@@ -1,13 +1,11 @@
 
 <?php
 
- use PHPMailer\PHPMailer\PHPMailer;
- use PHPMailer\PHPMailer\Exception;
 
- require 'C:\xampp\htdocs\Tangler/PHPMailer\src\Exception.php';
- require 'C:\xampp\htdocs\Tangler/PHPMailer\src\PHPMailer.php';
- require 'C:\xampp\htdocs\Tangler/PHPMailer\src\SMTP.php';
  if (isset($_POST['sendemailbutton'])) {
+  require 'PHPMailer/PHPMailerAutoload.php';
+  
+
  $name = $_POST['Firstntosendemail'];
  $fname=  $_POST['Lastntosendemail'];
  $from = $_POST['Emailsend'];
@@ -15,38 +13,34 @@
  $msg = $_POST['texttosendemail'];
  $n= $name . " " . $fname;
 
+ 
  $mail = new PHPMailer;
-
- $mail->isSMTP();                                      // Set mailer to use SMTP
- $mail->Host = 'tanglerinfor@gmail.com';  // Specify main and backup SMTP servers
+ $mail->Host = 'smtp.gmail.com"';  // Specify main and backup SMTP servers
+ $mail->Port = 465;
  $mail->SMTPAuth = true;                               // Enable SMTP authentication
+ $mail->SMTPSecure = 'ssl';                            // Enable encryption, 'ssl' also accepted
+ $mail->isSMTP();    
  $mail->Username = 'tanglerinfor@gmail.com';                 // SMTP username
  $mail->Password = 'RamiLeen';                           // SMTP password
- $mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
 
- $mail->From = $_POST['Emailsend'];
- $mail->FromName = $n;
- $mail->addAddress('tanglerinfor@gmail.com');     // Add a recipient
- //$mail->addAddress('ellen@example.com');               // Name is optional
- //$mail->addReplyTo('info@example.com', 'Information');
- //$mail->addCC('cc@example.com');
- //$mail->addBCC('bcc@example.com');
+ $mail->SetFrom($from, $n);
+ $mail->addAddress('rami.habash99@gmail.com');     // Add a recipient
+ $mail->addReplyTo($from, $n);
 
- $mail->WordWrap = 70;                                   // Set word wrap to 70 characters
- /*$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
- $mail->addAttachment('/tmp/image.jpg', 'new.jpg');      // Optional name */
- $mail->isHTML(true);                                    // Set email format to HTML
+ $mail->isHTML(true);  
 
  $mail->Subject = 'Reporting a problem';
- $mail->Body    = $msg;
- /*$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';*/
+ $mail->Body ='<h1>Name: '.$n. '<br>Email from: '. $from.'
+ <br>Message: '.$msg.'</h1>'; 
+
+
 
   if(!$mail->send()) {
     echo 'Message could not be sent.';
     echo 'Mailer Error: ' . $mail->ErrorInfo;
   } 
   else {
-    echo 'Message has been sent';
+    echo 'Thanks'.$n."for contacting us. We'll get back to you soon!";
   }
 }
 ?>
